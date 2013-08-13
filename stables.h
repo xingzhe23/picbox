72f8f5b872f31378bb05584aacb20d45244570a8
xingzhe23@163.com <xingzhe23@163.com>
2013-08-13 11:22:07 +0800

    add stables.h

---
+++ stables.h
@@ -0,0 +1,34 @@
+#ifndef _STABLES_H
+#define _STABLES_H
+
+
+#include <QMap>
+#include <QString>
+#include <QVector>
+#include <QSet>
+#include <QVarLengthArray>
+
+#include <QGraphicsWidget>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsItem>
+#include <QGraphicsLayoutItem>
+#include <QWidget>
+
+#include <QRegion>
+#include <QRect>
+#include <QRectF>
+
+#include <QPainter>
+#include <QPaintEngine>
+
+#include <QtCore/qmath.h>
+
+#include <QApplication>
+#include <QCoreApplication>
+
+//#windows
+#include <windows.h>
+#include <assert.h>
+
+#endif
